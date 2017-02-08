270e66c00e44a3f5e1c6870d31f0eff7d78e5818
dison ruan <dison.ruan@yahoo.com>
2017-02-08 01:13:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length-1] == "."
   end
 
   def question?
-
+    self[self.length-1] == "?"
   end
 
   def exclamation?
-
+    self[self.length-1] == "!"
   end
 
   def count_sentences
-
+    count = 0
+    sentence_end = ["!", "?", "."]
+    for i in 1...self.length
+      count += 1 if sentence_end.include?(self[i]) && !sentence_end.include?(self[i-1])
+    end
+    count
   end
 end
